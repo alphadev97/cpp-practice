2abbfd19b51c2761a4848d22a5256439d81442d7
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-02-13 01:22:00 +0500

    Comments

---
+++ 2.FirstSteps/2.2.Comments/main.cpp
@@ -0,0 +1,14 @@
+// This brings in the iostream library
+#include <iostream>
+
+/*
+    This is a block comment that englobes multiple
+    lines of text
+*/
+
+int main() {
+    // This is going to print "Hello world! to the console"
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+    // Programs ends here
+}
