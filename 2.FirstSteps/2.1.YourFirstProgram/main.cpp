f37927f6ca25c87639ec42d5cd744c0b444cd29f
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-02-13 01:13:31 +0500

    First program

--- 2.FirstSteps/2.1.YourFirstProgram/main.cpp
+++ 2.FirstSteps/2.1.YourFirstProgram/main.cpp
@@ -1,9 +1,9 @@
-#include <iostream> // iostream help us to print in console
+#include <iostream> // iostream help us to print in console, it is a c++ liabrary.
 
 int main() { // this is a function, and main entery point for our code to execute    
     std::cout << "Number1" << std::endl; // << std::endl is for line break
     std::cout << "Number2" << std::endl; // << these symbols are insertion operators
     std::cout << "Number3" << std::endl; // cout is a object stream of c++
 
-    return 0;
+    return 0; // for oprating system
 }
