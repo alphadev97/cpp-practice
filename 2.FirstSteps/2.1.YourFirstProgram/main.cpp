18b76f3847fd597ae5df5dd7fdb1e859ca5074f9
Muhammad Usama <116688082+alphadev97@users.noreply.github.com>
2024-02-13 01:10:57 +0500

    First program

---
+++ 2.FirstSteps/2.1.YourFirstProgram/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // iostream help us to print in console
+
+int main() { // this is a function, and main entery point for our code to execute    
+    std::cout << "Number1" << std::endl; // << std::endl is for line break
+    std::cout << "Number2" << std::endl; // << these symbols are insertion operators
+    std::cout << "Number3" << std::endl; // cout is a object stream of c++
+}
